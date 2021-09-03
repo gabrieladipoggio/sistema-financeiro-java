a0dc6ae28f384a7510e9e835aebdb549753d6787
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-09-03 10:47:32 -0300

    Adiciona métodos SistemaFinanceiro

--- src/SistemaFinanceiro.java
+++ src/SistemaFinanceiro.java
@@ -3,7 +3,8 @@ public class SistemaFinanceiro {
     public static void main(String[] args) {
         CaixaEletronico caixa = new CaixaEletronico();
         caixa.autenticar("2");
-
+        caixa.exibirSaldo();
+        caixa.encerrar();
         caixa.exibirSaldo();
 
     }
